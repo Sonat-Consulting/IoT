4517a673e9668f126243894368954f7d05e3864b
Sondre Eikanger Kvalø <zapodot@gmail.com>
2016-02-23 18:40:14 +0100

    oppgave template

--- oppgaver/ethernet/dhcp-sollution/dhcp-sollution.ino
+++ oppgaver/ethernet/dhcp-sollution/dhcp-sollution.ino
@@ -2,26 +2,26 @@
 #include <EtherCard.h>
 #include <net.h>
 
-byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+byte mymac[] = { 0x74, 0x69, 0x69, 0x2D, 0x30, 0x31 };
 byte Ethernet::buffer[700];
 boolean hasPrinted = false;
 
 void setup() {
   Serial.begin(9600);
-    if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
-      Serial.println( "Failed to access Ethernet controller");
-    if(!ether.dhcpSetup())
-    {
-      Serial.println("Could not obtain lease");
-    }
+  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
+    Serial.println( "Failed to access Ethernet controller");
+  if (!ether.dhcpSetup())
+  {
+    Serial.println("Could not obtain lease");
+  }
 
 }
 
 void loop() {
-  if(! hasPrinted) {
+  if (! hasPrinted) {
     ether.printIp("My IP: ", ether.myip);
     ether.printIp("GW IP: ", ether.gwip);
     ether.printIp("DNS IP: ", ether.dnsip);
     hasPrinted = true;
-  } 
+  }
 }
